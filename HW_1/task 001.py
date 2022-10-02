f9b73a019f4e561be658868bd18115ddd450bfcb
ilyanik77 <ilyakhlamov77@gmail.com>
2022-10-02 20:54:05 +0300

    task 001.py

---
+++ HW_1/task 001.py
@@ -0,0 +1,13 @@
+#  Напишите программу, которая принимает на вход цифру, обозначающую день недели
+#  и проверяет, является ли этот день выходным.
+
+a = int(input("Введите число от 0 до 7: "))
+if (a == 1): print("No")
+elif (a ==2): print("No")
+elif (a ==3): print("No")
+elif (a ==4): print("No")
+elif (a ==5): print("No")
+elif (a ==6): print("Yes")
+elif (a ==7): print("Yes")
+else : print("There is no such day")
+
