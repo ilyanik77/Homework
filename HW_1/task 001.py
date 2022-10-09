0fdef11470d45f478da2fd0ebfe294c1abfaf6f4
ilyanik77 <ilyakhlamov77@gmail.com>
2022-10-09 17:56:26 +0300

    Изменили решение HW_1/task 001.py

--- HW_1/task 001.py
+++ HW_1/task 001.py
@@ -2,12 +2,7 @@
 #  и проверяет, является ли этот день выходным.
 
 a = int(input("Введите число от 0 до 7: "))
-if (a == 1): print("No")
-elif (a ==2): print("No")
-elif (a ==3): print("No")
-elif (a ==4): print("No")
-elif (a ==5): print("No")
-elif (a ==6): print("Yes")
-elif (a ==7): print("Yes")
+if (a >= 1 and a <= 5): print("No")
+elif (a == 6 or a == 7): print("Yes")
 else : print("There is no such day")
 
